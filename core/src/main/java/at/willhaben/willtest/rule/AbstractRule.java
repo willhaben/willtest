a17cf2ab85df91ebc82d397e681053b6246a8229
Gabor Liptak <gabor.liptak@willhaben.at>
2017-01-27 14:59:28 +0100

    Documentation

--- core/src/main/java/at/willhaben/willtest/rule/AbstractRule.java
+++ core/src/main/java/at/willhaben/willtest/rule/AbstractRule.java
@@ -7,16 +7,36 @@ import org.junit.runners.model.Statement;
 /**
  * Similar to {@link org.junit.rules.ExternalResource}, but gives possibility to do something in case of errors.
  * Also if there is a test error, this class takes care, that errors from its implementations are added as
- * suppressed exceptions to this error to avoid loosing that original problem.
+ * suppressed exceptions to the test error to avoid loosing that original problem. If the test was successful, but
+ * {@link #after(Description, Throwable)} throws an error, then the test will fail.
  * Created by liptak on 2016.08.24..
  */
 public abstract class AbstractRule implements TestRule {
+    /**
+     * Can do something before ein test. By default it does nothing.
+     * @param description
+     * @throws Throwable
+     */
     protected void before(Description description) throws Throwable {
     }
 
+    /**
+     * Called after all tests always. By default it does nothing.
+     * @param description
+     * @param testFailure in case of failed test it contains an exception. Otherwise null
+     * @throws Throwable if <code>testFailure</code> is null, it will fly, if <code>testFailure</code> is not null,
+     * it will be added to that as suppressed exception. See {@link Throwable#addSuppressed(Throwable)}
+     */
     protected void after(Description description, Throwable testFailure) throws Throwable {
     }
 
+    /**
+     * Called if the test has thrown an error. By default it does nothing.
+     * @param description
+     * @param testFailure
+     * @throws Throwable this will be added to <code>testFailure</code> as suppressed exception.
+     * See {@link Throwable#addSuppressed(Throwable)}
+     */
     protected void onError(Description description, Throwable testFailure) throws Throwable {
     }
 
@@ -25,6 +45,11 @@ public abstract class AbstractRule implements TestRule {
         return new SafeStatement(description, base);
     }
 
+    /**
+     * {@link Statement} implementation, which takes care of calling {@link AbstractRule#before(Description)},
+     * {@link AbstractRule#after(Description, Throwable)}, {@link AbstractRule#onError(Description, Throwable)}, and
+     * handles their exceptions.
+     */
     private class SafeStatement extends Statement {
         private final Description description;
         private final Statement base;
