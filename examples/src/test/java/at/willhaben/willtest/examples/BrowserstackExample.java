9fe36f2f1e4f4f40c92d7f6c3cf57514ac1057dc
Michael Weisgrab <michael.weisgrab@willhaben.at>
2018-02-23 09:07:00 +0100

    added doc to Browserstack example

--- examples/src/test/java/at/willhaben/willtest/examples/BrowserstackExample.java
+++ examples/src/test/java/at/willhaben/willtest/examples/BrowserstackExample.java
@@ -4,16 +4,20 @@ import at.willhaben.willtest.misc.rule.SeleniumRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+/**
+ * How to use the BrowserstackSeleniumProvider
+ * You have to set the following vm-properties to run the test on Browserstack.
+ */
 public class BrowserstackExample {
-
     static {
-        System.setProperty("browserstack.hub", "");
-        System.setProperty("browserstack.local", "");
-        System.setProperty("platforms", "");
-        System.setProperty("platform.versions", "");
-        System.setProperty("browsers", "");
-        System.setProperty("browser.versions", "");
-        System.setProperty("display.resolution", "");
+        System.setProperty("seleniumProvider", "at.willhaben.willtest.browserstack.rule.BrowserstackSeleniumProvider");
+        System.setProperty("browserstack.hub", "https://<USERNAME>:<PASSWORD>@hub-cloud.browserstack.com/wd/hub");
+        System.setProperty("browserstack.local", "true");
+        System.setProperty("platforms", "OS X");
+        System.setProperty("platform.versions", "Sierra");
+        System.setProperty("browsers", "Chrome");
+        System.setProperty("browser.versions", "62.0");
+        System.setProperty("display.resolution", "1920x1080");
     }
 
     @Rule
