1069dc84aa663ebebfa98af96de79909c5812c34
Michael Weisgrab <michael.weisgrab@willhaben.at>
2018-02-23 08:32:34 +0100

    added vm properties to Browserstack example

--- examples/src/test/java/at/willhaben/willtest/examples/BrowserstackExample.java
+++ examples/src/test/java/at/willhaben/willtest/examples/BrowserstackExample.java
@@ -6,6 +6,16 @@ import org.junit.Test;
 
 public class BrowserstackExample {
 
+    static {
+        System.setProperty("browserstack.hub", "");
+        System.setProperty("browserstack.local", "");
+        System.setProperty("platforms", "");
+        System.setProperty("platform.versions", "");
+        System.setProperty("browsers", "");
+        System.setProperty("browser.versions", "");
+        System.setProperty("display.resolution", "");
+    }
+
     @Rule
     public SeleniumRule rule = new SeleniumRule();
 
