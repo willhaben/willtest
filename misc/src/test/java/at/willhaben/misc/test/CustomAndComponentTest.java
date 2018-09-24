0a3a349633b2da7d0266b504d156ca827d61be00
Michael Weisgrab <michael.weisgrab@willhaben.at>
2018-09-24 14:22:41 +0200

    renamed test

--- misc/src/test/java/at/willhaben/misc/test/CustomAndComponentTest.java
+++ misc/src/test/java/at/willhaben/misc/test/CustomAndComponentTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class TestCustomAnnotation {
+public class CustomAndComponentTest {
 
     WebDriver driver;
 
