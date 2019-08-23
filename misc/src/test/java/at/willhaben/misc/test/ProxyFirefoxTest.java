4c86d5c8dd97922ea4eb46ce507a3a7032814e90
Michael Weisgrab <michael.weisgrab@willhaben.at>
2019-08-23 09:40:56 +0200

    add firefox to travix yaml, start to implement test for proxy with firefox

---
+++ misc/src/test/java/at/willhaben/misc/test/ProxyFirefoxTest.java
@@ -0,0 +1,8 @@
+package at.willhaben.misc.test;
+
+import at.willhaben.willtest.junit5.extensions.DriverParameterResolver;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(DriverParameterResolver.class)
+public class ProxyFirefoxTest {
+}
