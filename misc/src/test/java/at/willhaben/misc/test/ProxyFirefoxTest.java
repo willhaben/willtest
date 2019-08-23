eba89a8dbf92144a4e36a5cc5240cf19729b759d
Michael Weisgrab <michael.weisgrab@willhaben.at>
2019-08-23 09:52:03 +0200

    update pom for firefox, update firefox test

--- misc/src/test/java/at/willhaben/misc/test/ProxyFirefoxTest.java
+++ misc/src/test/java/at/willhaben/misc/test/ProxyFirefoxTest.java
@@ -1,8 +1,18 @@
 package at.willhaben.misc.test;
 
 import at.willhaben.willtest.junit5.extensions.DriverParameterResolver;
+import at.willhaben.willtest.proxy.ProxyWrapper;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
 
 @ExtendWith(DriverParameterResolver.class)
-public class ProxyFirefoxTest {
+@Tag("phantomtest")
+class ProxyFirefoxTest {
+
+    @Test
+    void testProxy(WebDriver driver, ProxyWrapper proxyWrapper) {
+        driver.get("http://www.google.at");
+    }
 }
