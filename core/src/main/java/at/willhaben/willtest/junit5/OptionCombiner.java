531295d3601b8c29f9b2275b09e32be15011ff1e
Michael Weisgrab <michael.weisgrab@willhaben.at>
2019-10-17 15:49:55 +0200

    execute all option modifier only once

--- core/src/main/java/at/willhaben/willtest/junit5/OptionCombiner.java
+++ core/src/main/java/at/willhaben/willtest/junit5/OptionCombiner.java
@@ -30,18 +30,23 @@ public class OptionCombiner {
                     "created. May this is not a valid option class for the supported browsers.");
         }
         for (OptionModifier modifier : optionModifiers) {
-            options = modifier.modifyAllBrowsers(options);
             if (optionType.isAssignableFrom(FirefoxOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyFirefoxOptions(((FirefoxOptions) options));
             } else if (optionType.isAssignableFrom(ChromeOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyChromeOptions(((ChromeOptions) options));
             } else if (optionType.isAssignableFrom(EdgeOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyEdgeOptions(((EdgeOptions) options));
             } else if (optionType.isAssignableFrom(InternetExplorerOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyInternetExplorerOptions(((InternetExplorerOptions) options));
             } else if (optionType.isAssignableFrom(AndroidOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyAndroidOptions(((AndroidOptions) options));
             } else if (optionType.isAssignableFrom(IOsOptions.class)) {
+                options = modifier.modifyAllBrowsers(options);
                 options = (T) modifier.modifyIOsOptions(((IOsOptions) options));
             } else {
                 throw new BrowserNotSupportedException("The options class [" + optionType.getName() +
